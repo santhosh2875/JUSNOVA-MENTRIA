10a1cbcf51f7cd401cc2da964e7a60ab20f5c289
santhosh kumar <santhoshkumarbsk2875@gmail.com>
2025-09-05 12:46:17 +0530

    Update route.ts

--- app/admin/logout/route.ts
+++ app/admin/logout/route.ts
@@ -1,11 +1,6 @@
 import { NextResponse } from "next/server";
 import { cookies } from "next/headers";
 
-export async function GET(req: Request) {
-  cookies().delete("admin_session");
-  const url = new URL("/admin/login", req.url);
-  return NextResponse.redirect(url);
-}
 
 export async function POST(req: Request) {
   cookies().delete("admin_session");
